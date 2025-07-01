2f70726209714bcd912d3923644e466be8de04de
Freemir <54460730+Freemir@users.noreply.github.com>
2025-07-01 10:03:32 +0600

    Update files
    ...

---
+++ SOLID/SOLID/Program.cs
@@ -0,0 +1,111 @@
+﻿//Single Resp. - Единственная обязанность
+
+//abstract class Tech
+//{
+//    public abstract void sms();
+//    public abstract void call();
+//    public abstract void photo();
+//}
+
+//class Phone : Tech
+//{
+//    public void sms()
+//    {
+
+//    }
+//    public void call()
+//    {
+
+//    }
+//}
+
+
+//Open/Closed - Открыто для считывания, но закрыто для модификации
+//class Phone
+//{
+//    private string name;
+//    public string Name() => name;
+//}
+
+//L - Прицнип Лисков (Грамотный полиморфизм) Можно заменить главный класс одним из
+//его подтипов
+
+//abstract class Person
+//{
+//    public void move()
+//    {
+
+//    }
+//    public abstract void attack();
+//}
+
+//class Mage : Person {
+//    public override void attack()
+//    {
+
+//    }
+//}
+
+//class Mage : Person
+//{
+//    public override void attack()
+//    {
+
+//    }
+//}
+
+//Interface Разделение - прицнип того, чтобы интерфейсы не заставля классы реализовывать не нуные для них свойства
+
+//interface IHero
+//{
+//    public void swordAttack();
+//    public void mageDamage();
+//}
+
+//interface IKnight
+//{
+//    public void swordAttack();
+
+//}
+
+//interface IMage
+//{
+//    public void mageDamage();
+//}
+
+//class Knight : IHero
+//{
+//    public void mageDamage()
+//    {
+
+//    }
+//    public void swordAttack()
+//    {
+
+//    }
+
+//}
+
+//Dependecy - Логическая связь между сущносятми
+
+//DRY - Не повторяйся
+
+//KISS - Сделай это проще
+
+//YAGNI - От мелкого к большому
+
+
+//Directions d = Directions;
+
+//if (d == Directions.)
+//{
+//    Console.WriteLine(d);
+//}
+
+//enum Directions
+//{
+//    left,
+//    right,
+//    bottom,
+//    down
+//}
